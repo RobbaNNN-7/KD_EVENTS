1b9eac3ff28ed9786a7596ed1467f1428a276c0a
Khubaib Ahmed <kahmed.bscs23seecs@seecs.edu.pk>
2025-01-29 18:20:41 +0500

    fixed about section

--- script.js
+++ script.js
@@ -21,6 +21,21 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    $(document).ready(function(){
+        $("img").click(function(){
+        var t = $(this).attr("src");
+        $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+        $("#myModal").modal();
+      });
+      
+      $("video").click(function(){
+        var v = $("video > source");
+        var t = v.attr("src");
+        $(".modal-body").html("<video class='model-vid' controls><source src='"+t+"' type='video/mp4'></source></video>");
+        $("#myModal").modal();  
+      });
+      });//EOF Document.ready
+
     
 
     // 3. Contact Modal
@@ -42,6 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 modal.style.display = "none";
             }
         });
+
     }
 
     // 4. FAQ Dropdown Logic
